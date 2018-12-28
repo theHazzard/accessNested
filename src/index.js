6ba7fb979002ed543750096e562e4312faba6f03
Enzo Luis Strongoli <thehazzard@gmail.com>
2018-12-28 16:30:48 -0300

    feat(initial experiment): test

---
+++ src/index.js
@@ -0,0 +1,36 @@
+function isObject(variable) {
+    return typeCheck(variable, 'object');
+}
+
+function isArray(variable) {
+    return typeCheck(variable, 'array');
+}
+
+function typeCheck(variable, type) {
+    const regx = /\[\w+?\ (\w+)]/;
+    const description = Object.prototype.toString.call(variable);
+    const [, varType] = regx.match(description);
+
+    return varType.toLowerCase() === type.toLowerCase();
+}
+
+exports default function(container) {
+    if (!isArray(container) || !isObject(container)) {
+        throw Error('Must be called only on a object or array');
+    }
+    return function(...steps) {
+        let value;
+        try {
+            value = steps.reduce((dataStructure, step) => {
+                if (dataStructure[step] === undefined) {
+                    throw new Error('cannot traverse anymore');
+                }
+                return dataStructure[step];
+            }, container);
+        } catch (err) {
+            console.log('error');
+            value = undefined;
+        }
+        return value;
+    }
+}
