9a22bbff47c4a3bc822d94c7f3765a9b6a8e5c80
Enzo Luis Strongoli <thehazzard@gmail.com>
2019-01-31 12:37:44 -0300

    fix(fixes some errors):

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
 function isObject(variable) {
-    return typeCheck(variable, 'object');
+  return typeCheck(variable, 'object');
 }
 
 function isArray(variable) {
-    return typeCheck(variable, 'array');
+  return typeCheck(variable, 'array');
 }
 
 function typeCheck(variable, type) {
-    const regx = /\[\w+?\ (\w+)]/;
-    const description = Object.prototype.toString.call(variable);
-    const [, varType] = regx.match(description);
+  const regx = /\[\w+? (\w+)]/;
+  const description = Object.prototype.toString.call(variable);
+  const [, varType] = description.match(regx);
 
-    return varType.toLowerCase() === type.toLowerCase();
+  return varType.toLowerCase() === type.toLowerCase();
 }
 
-export default function(container) {
-    if (!isArray(container) || !isObject(container)) {
-        throw Error('Must be called only on a object or array');
-    }
-    return function(...steps) {
-        let value;
-        try {
-            value = steps.reduce((dataStructure, step) => {
-                if (dataStructure[step] === undefined) {
-                    throw new Error('cannot traverse anymore');
-                }
-                return dataStructure[step];
-            }, container);
-        } catch (err) {
-            console.log('error');
-            value = undefined;
+function check(container) {
+  if (!isArray(container) && !isObject(container)) {
+    throw Error('Must be called only on a object or array');
+  }
+  return function(...steps) {
+    let value;
+    try {
+      value = steps.reduce((dataStructure, step) => {
+        if (dataStructure[step] === undefined) {
+          throw new Error('cannot traverse anymore');
         }
-        return value;
+        return dataStructure[step];
+      }, container);
+    } catch (err) {
+      value = undefined;
     }
+    return value;
+  };
 }
